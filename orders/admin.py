ea68003748124415aa30736fd888e2960992b09b
Daniel Kang <danielkang674@gmail.com>
2018-07-27 16:59:35 -0500

    Models and admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Size, PizzaTopping, Pizza, SubTopping, Sub, Pasta, Salad, Platter
 
 # Register your models here.
+
+admin.site.register(Size)
+admin.site.register(PizzaTopping)
+admin.site.register(Pizza)
+admin.site.register(SubTopping)
+admin.site.register(Sub)
+admin.site.register(Pasta)
+admin.site.register(Salad)
+admin.site.register(Platter)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
 # Create your models here.
+
+class Size(models.Model):
+  size = models.CharField(max_length=6)
+
+  def __str__(self):
+    return f"{self.size}"
+
+class PizzaTopping(models.Model):
+  topping = models.CharField(max_length=25)
+
+  def __str__(self):
+    return f"{self.topping}"
+
+class Pizza(models.Model):
+  style = models.CharField(max_length=10)
+  type = models.CharField(max_length=12)
+  size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name="pizzaSize")
+  toppings = models.ManyToManyField(PizzaTopping, blank=True, related_name="toppings")
+
+  def __str__(self):
+    # pylint: disable=E1101
+    return f"{self.id} - {self.size} {self.style} {self.type} with {self.toppings}"
+
+class SubTopping(models.Model):
+  topping = models.CharField(max_length=25)
+
+  def __str__(self):
+    return f"{self.topping}"
+
+class Sub(models.Model):
+  type = models.CharField(max_length=40)
+  size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name="subSize")
+  toppings = models.ManyToManyField(SubTopping, blank=True, related_name="toppings")
+
+  def __str__(self):
+    # pylint: disable=E1101
+    return f"{self.id} - {self.size} {self.type} with {self.toppings}"
+
+class Pasta(models.Model):
+  type = models.CharField(max_length=35)
+
+  def __str__(self):
+    # pylint: disable=E1101
+    return f"{self.id} - {self.type}"
+
+class Salad(models.Model):
+  type = models.CharField(max_length=35)
+
+  def __str__(self):
+    # pylint: disable=E1101
+    return f"{self.id} - {self.type}"
+
+class Platter(models.Model):
+  type = models.CharField(max_length=35)
+  size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name="platterSize")
+
+  def __str__(self):
+    # pylint: disable=E1101
+    return f"{self.id} - {self.size} {self.type}"
